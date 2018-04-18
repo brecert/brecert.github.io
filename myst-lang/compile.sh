ba9e3c10ebe4f29ea0248b88ec9641c5d60220e0
Brecert <brecert@memework.org>
2018-04-18 14:00:11 -0400

    Initial commit

---
+++ myst-lang/compile.sh
@@ -0,0 +1,8 @@
+pug src --out ./public --watch & stylus src --out ./public --watch
+# node-sass src --output . ./public --watch
+# stylus src --out ./public --watch
+# Actual    : #2D4752 NO#4f7f93
+# Gravit    : #273F4A
+# Displayed : 79.05 127.5 147.9 (NO#4f8094)
+# Chrome    : 45.9, 71.4, 81.6 (#233840) (NO#467182)
+# Firefox   : 51, 81.6, 91.8 (#273F4A) (NO#518299)
