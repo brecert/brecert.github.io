e1493b3bdd9dda57d95113db7236239e26f0b36e
Brecert <brecert@memework.org>
2018-04-18 20:43:24 -0400

    Fixed a bunch of stuff and added really bad md support

--- myst-lang/.old.watch.sh
+++ myst-lang/.old.watch.sh
@@ -1,6 +1,12 @@
-pug src --out ./public --watch & stylus src --out ./public --watch
+#ruby ./listener.rb & stylus ./src --out ./public --watch && kill $!
+
+ruby ./listener.rb & pid1="$!" & stylus ./src --out ./public --watch
+
+#pug ./src --obj ./src/vars.js --out ./public --watch & stylus ./src --out ./public --watch
 # node-sass src --output . ./public --watch
 # stylus src --out ./public --watch
+# npm install --save jstransformer-markdown-it
+
 # Actual    : #2D4752 NO#4f7f93
 # Gravit    : #273F4A
 # Displayed : 79.05 127.5 147.9 (NO#4f8094)
---
+++ myst-lang/src/variables.rb
@@ -0,0 +1,33 @@
+name = 'Myst'
+
+{
+  name: name,
+  version: '0.0.6',
+
+  title: "#{name} - A structured, dynamic, general-purpose language",
+
+  statement: 'A language for clarity from the start, flexibility when you need it, and efficiency where it matters.',
+  example: 'IO.puts "Hello World!"',
+
+  description: """markdown:
+  Myst is a new programming language with the goal of bridging flexibility and practicality. By directly addressing common traps in modern dynamic programming, Myst provides users the ability to work quickly and easily with the power to write controlled, highly-structured code.
+
+  With heavy influences from Ruby, Elixir, Crystal, and other modern languages, Myst combines proven concepts like pattern matching and modular composition with some novel ideas, including global interpolation and fully optional typing. The result is a language that caters to natural thought and logic.
+
+  Some of the high-level features of Myst include:
+
+  > **Pattern-matching everywhere.** Assignments, method parameters, rescue clauses, etc.
+
+  > **Multi-clause functions.** All functions can define multiple clauses to adapt functionality based on inputs.
+
+  > **Value interpolations.** Interpolate any value anywhere (even in method parameters) with the <> syntax.
+
+  > **Soft typing.** Optional type annotations help control functionality without the clutter of conditionals.
+
+  > **Raise anything.** Any value can be raised as an exception and pattern matched by a rescue clause.
+
+  Myst is still very early on in its development. While the syntax stabilizing, there's still plenty of room for improvement, and even more openness to new ideas.
+
+  If you'd like to help shape the language, be an early adopter, or just follow along as things develop, it'd be great to see you in our discord server or on GitHub .
+  """
+}
